e741b1895d840d5d04646b3cf8557cbc9ac2709f
Vagram_ <vdourmychev@gmail.com>
2022-03-16 18:30:21 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
+    int c;
+    int a;
+    int b;
+
     public static void main(String[] args){
         comparison();
     }
+    public static void getNumber(){
+        int firstNumber = new Scanner(System.in).nextInt();
+        int secondNumber = new Scanner(System.in).nextInt();
+    }
     public static void comparison(){
-        int a = new Scanner(System.in).nextInt();
-        int b = new Scanner(System.in).nextInt();
         if (a < b) {
             System.out.println(a);
         }else System.out.println(b);
